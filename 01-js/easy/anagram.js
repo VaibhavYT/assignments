9b739a9f53940b9c66d3345f2ef6bb4559c6d212
Vaibhav Jaiswal <vaibhav.jaiswal.vj91@gmail.com>
2024-07-26 16:45:45 +0530

    anagram solved

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -1,11 +1,44 @@
-/*
-  Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
-  What's Anagram?
-  - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
-*/
+const charCountMap = new Map();
 
 function isAnagram(str1, str2) {
+  
+  const preprocess = (str) => str.toLowerCase().replace(/\s+/g, '');
 
+  const normalizedStr1 = preprocess(str1);
+  const normalizedStr2 = preprocess(str2);
+
+  if (normalizedStr1.length !== normalizedStr2.length) {
+    return false;
+  }
+
+  
+
+  
+  for (const char of normalizedStr2) {
+    charCountMap.set(char, (charCountMap.get(char) || 0) + 1);
+  }
+
+  
+  for (const char of normalizedStr1) {
+    if (!charCountMap.has(char)) {
+      return false;
+    }
+    charCountMap.set(char, charCountMap.get(char) - 1);
+    if (charCountMap.get(char) < 0) {
+      return false;
+    }
+  }
+
+  
+  for (const count of charCountMap.values()) {
+    if (count !== 0) {
+      return false;
+    }
+  }
+
+  return true;
 }
-
+if(isAnagram("hello","hello")){
+  console.log(charCountMap.values());
+}
 module.exports = isAnagram;
